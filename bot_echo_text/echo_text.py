f9720bde24f5cd833fe85b46aa47f228bdc7c327
Ke Jie <chzealot@gmail.com>
2023-09-19 14:00:00 +0800

    feat: 添加 Python 的机器人教程
    Signed-off-by: Ke Jie <chzealot@gmail.com>

---
+++ bot_echo_text/echo_text.py
@@ -0,0 +1,55 @@
+# !/usr/bin/env python
+
+import argparse
+import logging
+from dingtalk_stream import AckMessage
+import dingtalk_stream
+
+def setup_logger():
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter('%(asctime)s %(name)-8s %(levelname)-8s %(message)s [%(filename)s:%(lineno)d]'))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+def define_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--client_id', dest='client_id', required=True,
+        help='app_key or suite_key from https://open-dev.digntalk.com'
+    )
+    parser.add_argument(
+        '--client_secret', dest='client_secret', required=True,
+        help='app_secret or suite_secret from https://open-dev.digntalk.com'
+    )
+    options = parser.parse_args()
+    return options
+
+
+class EchoTextHandler(dingtalk_stream.ChatbotHandler):
+    def __init__(self, logger: logging.Logger = None):
+        super(dingtalk_stream.ChatbotHandler, self).__init__()
+        if logger:
+            self.logger = logger
+
+    async def process(self, callback: dingtalk_stream.CallbackMessage):
+        incoming_message = dingtalk_stream.ChatbotMessage.from_dict(callback.data)
+        text = incoming_message.text.content.strip()
+        self.reply_text(text, incoming_message)
+        return AckMessage.STATUS_OK, 'OK'
+
+def main():
+    logger = setup_logger()
+    options = define_options()
+
+    credential = dingtalk_stream.Credential(options.client_id, options.client_secret)
+    client = dingtalk_stream.DingTalkStreamClient(credential)
+    client.register_callback_handler(dingtalk_stream.chatbot.ChatbotMessage.TOPIC, EchoTextHandler(logger))
+    client.start_forever()
+
+
+if __name__ == '__main__':
+    main()
